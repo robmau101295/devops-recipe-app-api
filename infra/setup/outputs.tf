f4c9998ed05c5d4f222994a43246a200701074b4
Robert Orozco <mauricio_101295@outlook.com>
2026-08-25 10:07:28 -0500

    stup github actions

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -1,12 +1,21 @@
 output "cd_user_access_key_id" {
-    description = "Access key ID for the CD user"
-    value       = aws_iam_access_key.cd.id
+  description = "Access key ID for the CD user"
+  value       = aws_iam_access_key.cd.id
 }
 
-output  "cd_user_secret_access_key" {
-    description = "Secret access key for the CD user"
-    value       = aws_iam_access_key.cd.secret
-    sensitive   = true
+output "cd_user_secret_access_key" {
+  description = "Secret access key for the CD user"
+  value       = aws_iam_access_key.cd.secret
+  sensitive   = true
 }
 
+output "ecr_repo_app" {
+  description = "ECR repository URL for the app"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxy" {
+  description = "ECR repository URL for the proxy"
+  value       = aws_ecr_repository.proxy.repository_url
+}
 
